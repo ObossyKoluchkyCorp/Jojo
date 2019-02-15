b1119a171c94d08b8f324d3c8fd9deb2d3b3b164
Papupoca <45541945+Papupoca@users.noreply.github.com>
2019-02-16 02:08:40 +0300

    0.3

---
+++ JojoMode/Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+ // Egor say boob
+    void Update()
+    {
+        
+    }
+}
